bb0900e8196ec70fddae8c935a0954a3a62c0532
Arslan19955 <arslan@gsten.co>
2024-08-06 17:26:49 +0500

    add default fields

--- src/pages/fmsca-table/index.tsx
+++ src/pages/fmsca-table/index.tsx
@@ -28,8 +28,8 @@ interface RowData {
 }
 
 const defaultPivotState = {
-  rows: ['created_dt'], // Replace 'defaultRowField' with your actual field name
-  cols: ['legal_name'], // Replace 'defaultColField' with your actual field name
+  rows: ['created_dt', 'data_source_modified_dt', 'entity_type', 'operating_status', 'legal_name', 'dba_name', 'physical_address', 'phone', 'usdot_number', 'mc_mx_ff_number', 'power_units', 'out_of_service_date'], // Replace 'defaultRowField' with your actual field name
+  // cols: ['legal_name'], // Replace 'defaultColField' with your actual field name
   aggregatorName: 'Count', // Replace with your desired aggregator
   vals: [],
 };
